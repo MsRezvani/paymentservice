c9cd3277b6e91fc3b33f0cdc112d2eb331f9bb40
mostafa <beti.mazinani@gmail.com>
2020-11-02 11:51:28 +0330

    make change: ResourceNotFoundException -> NoSuchElementException

--- src/main/java/com/digipay/paymentservice/paymentservice/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.digipay.paymentservice.paymentservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
